31f77f162b4549116da49d64ccfe4ba75f2fac71
Mike Heffner <mikeh@fesnel.com>
2025-06-06 17:00:22 -0400

    Update tagline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Rotel',
-  tagline: 'Lightweight OpenTelemetry collector written in Rust',
+  tagline: 'High Performance, Resource Efficient OpenTelemetry Collector',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
